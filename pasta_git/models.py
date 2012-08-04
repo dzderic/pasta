555e167b8f711593fd5d89afbb2d0ca3b24b6399
David Zderic <dzderic@gmail.com>
2012-08-04 14:53:46 +1000

    Implemented primitive forking

--- pasta_git/models.py
+++ pasta_git/models.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 
 from django.db import models
 from django.conf import settings
@@ -7,6 +8,7 @@ from django.contrib.auth.models import User
 class Repository(models.Model):
     owner = models.ForeignKey(User)
     name = models.CharField(max_length=255)
+    fork_of = models.ForeignKey('self', null=True)
 
     class Meta(object):
         permissions = (
@@ -17,5 +19,11 @@ class Repository(models.Model):
     @property
     def path(self):
         return os.path.join(settings.REPOSITORY_HOME, self.owner.username, self.name + '.git')
+
+    def fork(self, recipient):
+        new_repo = Repository(owner=recipient, name=self.name, fork_of=self)
+        shutil.copytree(self.path, new_repo.path)
+        new_repo.save()
+        return new_repo
 
 from pasta_git.signals import *
--- pasta_git/tests.py
+++ pasta_git/tests.py
@@ -11,21 +11,28 @@ from pasta_git.models import Repository
 
 class RepositoryTestCase(unittest.TestCase):
     def setUp(self):
-        self._some_guy, _ = User.objects.get_or_create(username='some-guy', password='password')
+        self._human_a, _ = User.objects.get_or_create(username='human-a', password='password')
+        self._human_b, _ = User.objects.get_or_create(username='human-b', password='password')
         settings.REPOSITORY_HOME = tempfile.mkdtemp()
 
     def tearDown(self):
         shutil.rmtree(settings.REPOSITORY_HOME)
 
     def test_creating_model_creates_repository(self):
-        repo = Repository(owner=self._some_guy, name='awesome-repository')
+        repo = Repository(owner=self._human_a, name='awesome-repository')
         self.assertFalse(os.path.exists(repo.path))
         repo.save()
         self.assertTrue(os.path.exists(repo.path))
 
     def test_deleting_model_archives_repository(self):
-        repo = Repository(owner=self._some_guy, name='another-repository')
+        repo = Repository(owner=self._human_a, name='another-repository')
         repo.save()
         self.assertTrue(os.path.exists(repo.path))
         repo.delete()
         self.assertTrue(os.path.exists('%s.archived.%s' % (repo.path, int(time.time()))))
+
+    def test_forking_works(self):
+        repo = Repository(owner=self._human_a, name='some-code')
+        repo.save()
+        fork = repo.fork(recipient=self._human_b)
+        self.assertTrue(os.path.exists(fork.path))
